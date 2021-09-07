bfb528ea6ae88a95a2916f770a79943027d36ef1
monika <rajputmonika276@gmail.com>
2021-09-07 13:01:33 +0530

    add the notes

--- src/importantNotes/ImagesModal.jsx
+++ src/importantNotes/ImagesModal.jsx
@@ -1,8 +1,12 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
-import {itemData} from './data';
+import { itemData } from './data';
 import { GridListTile } from '@material-ui/core';
+import ShareIcon from '@material-ui/icons/Share';
+import ImageListItemBar from '@material-ui/core/ImageListItemBar';
+import IconButton from '@material-ui/core/IconButton';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,30 +20,45 @@ const useStyles = makeStyles((theme) => ({
     width: 800,
     height: 500,
   },
+  title: {
+    color: 'white',
+  }
 }));
 
 
- function ImagesModal(props) {
+function ImagesModal(props) {
   // setView, setFileSelect, fileSelect
-  console.log("image data",itemData)
-  const [ selectImage, setSelectImage] = useState("")
+  console.log("image data", itemData)
+  const [selectImage, setSelectImage] = useState("")
   const classes = useStyles();
- 
 
-  const handleImageClick = (id) =>{ 
-      props.setFileSelect(files => [...files,`${id}`])
-      console.log(props.fileSelect)
-  }
-  
+
+  // const handleImageClick = (id) =>{ 
+  //     props.setFileSelect(files => [...files,`${id}`])
+  //     console.log(props.fileSelect)
+  // }
+
   return (
     <div className={classes.root}>
       <GridList rowHeight={160} className={classes.imageList} cols={4} >
-        {itemData.map(item => 
+        {itemData.map(item =>
           <GridListTile key={item.id} cols={item.cols || 1} >
-            <img 
-            src={item.image}  
-            onClick={() => handleImageClick(item.id)}         
-             />
+            <img
+              src={item.image}
+            />
+            <ImageListItemBar
+              title={item.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton aria-label={`star ${item.title}`}>
+                  <ShareIcon className={classes.title} />
+                </IconButton>
+              }
+            />
+            {/* <ShareIcon fontSize="large" style={{color:'white',}}/> */}
           </GridListTile>
         )}
       </GridList>
